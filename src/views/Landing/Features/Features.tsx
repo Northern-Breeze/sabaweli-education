87566bac2fd2254ad621a16871d952d72f062b71
Thagana <samuelmthwa79@gmail>
2021-03-08 18:01:13 +0200

    Landing page prices and information

---
+++ src/views/Landing/Features/Features.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Template from '../../Template';
+
+
+export default function Features() {
+    return (
+        <Template>
+            Hello
+        </Template>
+    )
+}
---
+++ src/views/Landing/Landing/Landing.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Template from '../../Template';
+
+
+import './Landing.scss';
+
+export default function Landing() {
+    return (
+        <Template>
+            Hello
+        </Template>
+    )
+}
