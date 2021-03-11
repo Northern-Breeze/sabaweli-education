89b75b6ba4ac766a1af47a75eadf3f3701907e25
Thagana <samuelmthwa79@gmail>
2021-03-11 21:50:28 +0200

    a service

--- src/service/server.ts
+++ src/service/server.ts
@@ -3,6 +3,7 @@ import Axios from './network';
 import IRegister from '../interfaces/Authentication/IRegister';
 import ILogin from '../interfaces/Authentication/ILogin';
 import IVerifyToken from '../interfaces/Authentication/IVerifyToken'
+import Itoken from '../interfaces/Authentication/Itoken';
 
 const Server = {
     // login the user
@@ -10,7 +11,9 @@ const Server = {
     // register the users
     registerUser: (data: IRegister) => Axios.post('/auth/register', data),
     // Validate verify token
-    validateToken: (data: IVerifyToken) => Axios.post('/auth/verify', data)
+    validateToken: (data: IVerifyToken) => Axios.post('/auth/verify', data),
+    // check token validity
+    verifyUser: (data: Itoken) => Axios.post('/auth/checkuser', data)
 }
 
 export default Server;
