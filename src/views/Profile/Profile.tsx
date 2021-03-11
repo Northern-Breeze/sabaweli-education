ab06ff844067d346ee293d951fb5c325d5dcb135
Thagana <samuelmthwa79@gmail>
2021-03-11 21:47:05 +0200

    Profile

--- src/views/Profile/Profile.tsx
+++ src/views/Profile/Profile.tsx
@@ -4,7 +4,13 @@ import Template from '../Template';
 export default function Profile() {
     return (
         <Template>
-            profile
+            <div className="profile-container">
+                <div className="profile-avatar">
+                    <div className="avatar">
+                        Avatar
+                    </div>
+                </div>
+            </div>
         </Template>
     )
 }
