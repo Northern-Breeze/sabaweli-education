267b5ddbbafc59f1760f6c0dcdea8eee36d42740
Thagana <samuelmthwa79@gmail>
2021-04-16 21:29:49 +0200

    private routes

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import {Redirect, Route, RouteProps, RouteComponentProps} from 'react-router'
 
 interface PrivateRouteProps extends RouteProps {
