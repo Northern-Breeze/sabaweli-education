0c991af32d0f54d87b73e308ba7a01f4ad57b62c
Thagana <samuelmthwa79@gmail>
2021-05-06 18:18:47 +0200

    views update

---
+++ src/views/Checkout/Form/helper/useOptions.tsx
@@ -0,0 +1,30 @@
+import { useMemo } from 'react';
+import useResponsiveFontSize from './useResponsiveFonts';
+import {  } from '@stripe/react-stripe-js'
+const useOptions = (): any => {
+    const fontSize = useResponsiveFontSize();
+    const options = useMemo(
+      () => ({
+        style: {
+          base: {
+            fontSize,
+            color: "#424770",
+            letterSpacing: "0.025em",
+            fontFamily: "Source Code Pro, monospace",
+            padding: '10px',
+            "::placeholder": {
+              color: "#000"
+            }
+          },
+          invalid: {
+            color: "#9e2146"
+          }
+        }
+      }),
+      [fontSize]
+    );
+  
+    return options;
+  };
+
+  export default useOptions;
