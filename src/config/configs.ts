883994c61a64622918eb5f2c01ab2b25015713f6
Thagana <samuelmthwa79@gmail>
2021-06-05 21:15:36 +0200

    checkout

--- src/config/configs.ts
+++ src/config/configs.ts
@@ -1,7 +1,7 @@
 const devConfig = {
     PAYFAST_URL: 'https://sandbox.payfast.co.za​/eng/process',
-    PAYFAST_RETURN_URL: 'https://1e9f6a213022.ngrok.io/checkout?payment-status=payment-success',
-    PAYFAST_CANCEL_URL: 'https://1e9f6a213022.ngrok.io/checkout?payment-status=payment-canceled',
+    PAYFAST_RETURN_URL: 'https://1e9f6a213022.ngrok.io/checkout?payment-status=success',
+    PAYFAST_CANCEL_URL: 'https://1e9f6a213022.ngrok.io/checkout?payment-status=canceled',
     MERCHANT_KEY: 'q1cd2rdny4a53',
     MERCHANT_ID: '10004002',
     PASSPHRASE: 'payfast',
@@ -11,8 +11,8 @@ const devConfig = {
 
 const prodConfigs = {
     PAYFAST_URL: 'https://www.payfast.co.za/eng/process',
-    PAYFAST_RETURN_URL: 'https://sabaweli.netlify.app/checkout?payment-status=payment-success',
-    PAYFAST_CANCEL_URL: 'https://sabaweli.netlify.app/checkout?payment-status=payment-canceled',
+    PAYFAST_RETURN_URL: 'https://sabaweli.netlify.app/checkout?payment-status=success',
+    PAYFAST_CANCEL_URL: 'https://sabaweli.netlify.app/checkout?payment-status=anceled',
     MERCHANT_KEY: process.env.REACT_APP_MERCHANT_KEY || "",
     MERCHANT_ID: process.env.REACT_APP_MERCHANT_ID || "",
     PASSPHRASE: process.env.PASSPHRASE,
