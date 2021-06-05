60a5d37c85e6e40e49b5bc5f904dd87b6a2bf2da
Thagana <samuelmthwa79@gmail>
2021-06-05 16:59:25 +0200

    Notfound page update

--- src/views/NotFound/NotFound.tsx
+++ src/views/NotFound/NotFound.tsx
@@ -1,9 +1,12 @@
 import * as  React from 'react';
+import Animation from '../../components/Animation';
+import animationFile from '../../assets/annimation/42479-page-not-found-404.json';
+import './Notfound.scss';
 
 export default function NotFound(): JSX.Element {
     return (
-        <div>
-            Page Not found
+        <div className="page-not-found">
+            <Animation fileObject={animationFile} />
         </div>
     )
 }
