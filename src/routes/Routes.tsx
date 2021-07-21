7c57732c131a84727eb29cf9c8655eaacf4b0272
Thagana <samuelmthwa79@gmail>
2021-07-21 21:59:37 +0200

    create question answer simulator

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -15,17 +15,13 @@ import Results from "../views/Profile/Results";
 import Audio2Notes from "../views/Profile/Audio2Notes";
 import ForgotPasswordRequest from "../views/Auth/FogotPasswordRequest";
 import ChangePassword from "../views/Auth/ChangePassword";
+import QuestionSimulator from "../views/Profile/QuestionSimulator";
 
 export default function Routes(): JSX.Element {
-
   return (
     <Router>
       <Switch>
-        <PrivateRoute
-          exact
-          path='/profile'
-          component={Profile}
-        />
+        <PrivateRoute exact path='/profile' component={Profile} />
         <PrivateRoute
           exact
           path='/profile/audio-2-notes'
@@ -33,14 +29,11 @@ export default function Routes(): JSX.Element {
         />
         <PrivateRoute
           exact
-          path='/results'
-          component={Results}
+          path='/profile/questions-simulator'
+          component={QuestionSimulator}
         />
-        <PrivateRoute
-          exact
-          path='/checkout'
-          component={Checkout}
-        />
+        <PrivateRoute exact path='/results' component={Results} />
+        <PrivateRoute exact path='/checkout' component={Checkout} />
         <Route exact path='/pricing' component={Pricing} />
         <Route exact path='/' component={Landing} />
         <Route exact path='/features' component={Features} />
--- src/views/Profile/Audio2Notes/Audio2Notes.tsx
+++ src/views/Profile/Audio2Notes/Audio2Notes.tsx
@@ -1,36 +1,84 @@
-import React from "react";
-import TemplateWrapper from "../../TemplateWrapper";
-import AudioToNotes from "../../../components/Modals/AudioToNotes";
-import Loading from "../../../components/Loading";
-import Button from '../../../components/Button';
+import * as React from "react";
+import { UploadOutlined } from "@ant-design/icons";
+import WaveSurfer from "wavesurfer.js";
 
-import './Audio2Notes.scss';
+// components
+import TemplateWrapper from "../../TemplateWrapper";
+import Loading from "../../../components/Loading";
+import Button from "../../../components/Button";
+
+import "./Audio2Notes.scss";
 
 export default function Audio2Notes(): JSX.Element {
-  const [open, setOpen] = React.useState(false);
   const [networkLoading, setNetworkLoading] = React.useState(false);
-  const setToggleModal = () => {
-    setOpen(!open);
+  const [file, setFile] = React.useState<File>();
+
+  const wavesurfer = React.useRef<WaveSurfer>();
+
+  function readFile(event: any) {      
+      const results = event.target.result;
+      const audio = new Audio();
+      audio.src = URL.createObjectURL(results);
+      if(wavesurfer.current){
+        // wavesurfer.current.load(audio);
+        wavesurfer.current.load('http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
+      }
+  }
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const fileList = event.target.files;
+    if (!fileList) return;
+    const mediaFile = fileList[0]
+    setFile(mediaFile); 
+    const reader = new FileReader();
+    reader.addEventListener('load', readFile);
   };
-  const setLoadingStatus = (value: boolean) => {
-    setNetworkLoading(value);
-  };
+
+  React.useEffect(() => {
+    wavesurfer.current = WaveSurfer.create({
+      container: "#waveform",
+      waveColor: "#f9627dff",
+      progressColor: "purple",
+      barWidth: 2,
+    })    
+  },[])
+
   if (networkLoading) {
     <TemplateWrapper>
-      <div className="converter-container">
+      <div className='converter-container'>
         <Loading />
       </div>
     </TemplateWrapper>;
   }
+
   return (
     <TemplateWrapper>
-      <div className="converter-container">
-        <Button onClick={setToggleModal} className="primary">Convert Audio 2 Notes</Button>
-        <AudioToNotes
-          openAudioModal={open}
-          setAudioConvModal={setToggleModal}
-          setNetworkLoading={setLoadingStatus}
-        />
+      <div className='wave-canvas'>
+        <div id='waveform'></div>
+      </div>
+      <div className='converter-container'>
+        <form className='form'>
+          <div className='field'>
+            <label className='custom-file-upload'>
+              <input
+                type='file'
+                accept='.mp4,.mp4,.wav,.flac'
+                name='file'
+                className='file-input'
+                onChange={handleFileChange}
+              />
+              <div className='icon-container'>
+                <UploadOutlined />
+              </div>
+              Select File
+            </label>
+          </div>
+          <div className='field'>
+            <Button className='primary' type='button'>
+              Upload Media
+            </Button>
+          </div>
+        </form>
       </div>
     </TemplateWrapper>
   );
