c8353f79944467251740101102c0d59ec117c156
Thagana <samuelmthwa79@gmail>
2022-02-17 18:19:07 +0200

    updating routing

--- src/views/Checkout/PaymentMethods/Stripe/Stripe.tsx
+++ src/views/Checkout/PaymentMethods/Stripe/Stripe.tsx
@@ -4,11 +4,12 @@ import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "../Form/CardForm";
 import { useLocation } from "react-router-dom";
 
-import './Stripe.scss';
+import "./Stripe.scss";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_DEV_KEY || "");
 
 interface LocationState {
+  state: {
     from: {
       pathname: string;
     };
@@ -16,30 +17,31 @@ interface LocationState {
     price: number;
     package: string;
     name: string;
-  }
+  };
+}
 
 export default function Stripe(): JSX.Element {
   const [price, setPrice] = React.useState(0);
   const [title, setTitle] = React.useState("");
   const [name, setName] = React.useState("");
-  const location = useLocation<LocationState>();
+  const location = useLocation();
   React.useEffect(() => {
-    const { state } = location;
+    const { state } = location as LocationState;
     setPrice(state?.price);
     setTitle(state?.package);
     setName(state?.name);
   }, [location]);
   return (
-    <div className="stripe-container">
-      <div className="form-checkout">
-        <div className="product-info">
+    <div className='stripe-container'>
+      <div className='form-checkout'>
+        <div className='product-info'>
           <div>
-            <h3 className="product-title">
+            <h3 className='product-title'>
               {title} - {name}
             </h3>
           </div>
           <div>
-            <h4 className="product-price">{`$ ${price}`}</h4>
+            <h4 className='product-price'>{`$ ${price}`}</h4>
           </div>
         </div>
         <Elements stripe={stripePromise}>
